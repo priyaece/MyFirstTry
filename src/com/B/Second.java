e15955f3ab3e8fd8ac2dcb826319a37dd9b8f174
priya <skppp7@gmail.com>
2016-10-12 20:21:57 +0530

    new added code in class Second of package B

--- src/com/B/Second.java
+++ src/com/B/Second.java
@@ -4,6 +4,7 @@ public class Second {
 
 	public static void main(String[] args) {
 	    System.out.println("Second class in B");
+	    System.out.println("my name is sanjeev in class Second in package in B");
 	}
 
 }
