d60bfff213b8dbe007dbf7c867607f4f44aacbeb
priya <skppp7@gmail.com>
2016-10-12 20:15:19 +0530

    vhdbjkfdvkfdjnvjhfvhg vfhjbvfjkvnfmnvfnvkfjmdnvjkskjnjdkncbjd

---
+++ src/com/A/First.java
@@ -0,0 +1,10 @@
+package com.A;
+
+public class First {
+
+	public static void main(String[] args) {
+	    System.out.println("First class in A");
+
+	}
+
+}
---
+++ src/com/B/Second.java
@@ -0,0 +1,9 @@
+package com.B;
+
+public class Second {
+
+	public static void main(String[] args) {
+	    System.out.println("Second class in B");
+	}
+
+}
